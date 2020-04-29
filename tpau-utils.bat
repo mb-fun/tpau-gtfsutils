daf5ee355ad98c2ad32a464d9638d609b1175b10
Annie Fifer <annie@trilliumtransit.com>
2020-04-29 14:49:41 -0700

    batch syntax

--- tpau-utils.bat
+++ tpau-utils.bat
@@ -1,12 +1,14 @@
+echo off
+
 CALL activate gtfsutils
 
 IF "%1" == ""^
- ECHO Utility argument required, please use one of the following:
- ECHO   average_headways
+ ECHO Utility argument required, please use one of the following:^ 
+    average_headways^
 
 ELSE IF "%1" != "average_headway" THEN^
- ECHO Utility argument not recognized, please use one of the following:
- ECHO   average_headways
+ ECHO Utility argument not recognized, please use one of the following:^
+    average_headways^
 
 ELSE python main.py %1
 
