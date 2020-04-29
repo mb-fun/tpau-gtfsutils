dc3fb6e62477b7f03d152ea9a9afa5e85ee45f8a
Annie Fifer <annie@trilliumtransit.com>
2020-04-29 15:06:48 -0700

    more batch

--- tpau-utils.bat
+++ tpau-utils.bat
@@ -2,16 +2,16 @@
 
 IF "%1" == "" (
     ECHO Utility argument required, please use one of the following:
-    ECHO   average_headways
-    EXIT
+    ECHO   average_headway
+    EXIT /b
 )
 
-IF "%1" != "average_headway" (
+IF NOT "%1" == "average_headway" (
     ECHO Utility argument not recognized, please use one of the following:
-    ECHO   average_headways
-    EXIT
+    ECHO   average_headway
+    EXIT /b
 )
 
 CALL activate gtfsutils
-python main.py %1
+python main.py -u %1
 CALL conda deactivate
