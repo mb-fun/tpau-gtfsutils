44b5575c8b13826719c124bb5f5024ec6942301d
Annie Fifer <annie@trilliumtransit.com>
2020-07-22 14:54:26 -0700

    reset feed dates

--- tpau_gtfsutilities/gtfs/methods/filters/subset.py
+++ tpau_gtfsutilities/gtfs/methods/filters/subset.py
@@ -1,5 +1,5 @@
 from tpau_gtfsutilities.gtfs.gtfssingleton import gtfs
-from tpau_gtfsutilities.gtfs.methods.filters.daterange import filter_calendars_by_daterange, filter_calendar_dates_by_daterange
+from tpau_gtfsutilities.gtfs.methods.filters.daterange import filter_calendars_by_daterange, filter_calendar_dates_by_daterange, reset_feed_dates
 from tpau_gtfsutilities.gtfs.methods.filters.timerange import filter_single_trips_by_timerange, filter_repeating_trips_by_timerange
 import tpau_gtfsutilities.gtfs.methods.filters.prune as prune
 
@@ -13,4 +13,4 @@ def subset_entire_feed(daterange, timerange=None):
     prune.prune_unused_stops()
     prune.prune_unused_routes()
     prune.prune_unused_shapes()
-    # reset_feed_dates(daterange)
+    reset_feed_dates(daterange)
