ce2b9d0f2acf3c59eabc574ce4a3eeae73e81735
Annie Fifer <annie@trilliumtransit.com>
2020-11-29 14:09:15 -0800

    test configs for average headways, cluster stops

--- run_tests.sh
+++ run_tests.sh
@@ -7,10 +7,13 @@ conda activate gtfsutils
 util=$1
 
 for f in testing/configs/*.yaml; do
-    if [[ $f =~ $util ]]
+    if [[ $f =~ $util ]] && [[ $f != *template* ]]
     then 
+        filename=$(basename -- "$f")
+        filename="${filename%.*}"
+        # output_file=testing/testing_output/$filename.output.txt
         echo "Running $util with config $f..."
-        python main.py -u $util -c $f -i testing/data/ -o testing/output/
+        python main.py -e -u $util -c $f -i testing/data/ -o testing/output/
     fi;
 done
 
