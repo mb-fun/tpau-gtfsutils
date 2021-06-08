9d173232d764fa2521876a4ae2e17006a2e1253d
Annie Fifer <annie@trilliumtransit.com>
2021-06-08 15:01:25 -0700

    environment yaml

--- build.bat
+++ build.bat
@@ -1,14 +1,5 @@
-rem # If you change this name be sure to remove any conda existing sessions
-call conda create -n gtfsutils python=3.7
+call conda env create -f environment.yml
 
 call conda activate gtfsutils
-
-rem #new install
-call conda install pandas
-call conda install geopandas
-call conda install pyyaml
-call conda install shapely
-
 python setup.py develop
-
 CALL conda deactivate
--- build.sh
+++ build.sh
@@ -1,11 +1,2 @@
-conda create -n gtfsutils python=3.7
-
-conda activate gtfsutils
-conda install pyyaml
-conda install pandas
-conda install geopandas
-conda install shapely
-
-python setup.py develop
-
-conda deactivate
+conda env create -f environment.yml
+conda activate gtfsutils; python setup.py develop; conda deactivate;
